7194cfc65340371392f3fa31988f5d9808b964c5
Pavan Sudheendra <pavan0591@gmail.com>
2022-10-21 11:17:46 +0100

    Update test

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"github.com/91pavan/twitter-connector/pkg/app"
+	"github.com/91pavan/twitter-connector/pkg/models"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -20,11 +21,50 @@ func TestGetRequest(t *testing.T) {
 	a.ListConnectorSpecification(w, r)
 
 	assert.Equal(t, http.StatusOK, w.Code)
-	resp := map[string]string{"API_KEY": "Access key",
-		"API_KEY_SECRET":      "Access key Secret",
-		"ACCESS_TOKEN":        "Access Token",
-		"ACCESS_TOKEN_SECRET": "Access Token Secret",
+	fields := &models.SpecificationV2{}
+
+	apiKey := models.FieldSpecificationV2{
+		Field:       "API_KEY",
+		Type:        "String",
+		MinLength:   3,
+		MaxLength:   40,
+		Required:    true,
+		Description: "Consumer Key",
+		Example:     "TmiVYoLyeUyX9MBdKjtqQZjCB",
 	}
-	res, _ := json.Marshal(resp)
+	fields.Fields = append(fields.Fields, apiKey)
+	apiSecret := models.FieldSpecificationV2{
+		Field:       "API_KEY_SECRET",
+		Type:        "String",
+		MinLength:   10,
+		MaxLength:   60,
+		Required:    true,
+		Description: "Consumer Secret",
+		Example:     "oA8f7fXLkZ7dKGukms1yFUNDdhlqd9koJNGfDaedpAoeaoepBd",
+	}
+	fields.Fields = append(fields.Fields, apiSecret)
+
+	accessToken := models.FieldSpecificationV2{
+		Field:       "ACCESS_TOKEN",
+		Type:        "String",
+		MinLength:   10,
+		MaxLength:   60,
+		Required:    true,
+		Description: "Twitter Developer Access Token",
+		Example:     "337265965-fPOgoWoSdSQxM8VE8GfvKzGYrWhX9WjliZIY0ENS",
+	}
+	fields.Fields = append(fields.Fields, accessToken)
+
+	accessTokenSecret := models.FieldSpecificationV2{
+		Field:       "ACCESS_TOKEN_SECRET",
+		Type:        "String",
+		MinLength:   10,
+		MaxLength:   60,
+		Required:    true,
+		Description: "Twitter Developer Access Token Secret",
+		Example:     "x4ZocH8PIYRFBF1bJJ7HgetSuGqk8NUNYspafEIX4cdi1",
+	}
+	fields.Fields = append(fields.Fields, accessTokenSecret)
+	res, _ := json.Marshal(fields)
 	assert.Equal(t, res, w.Body.Bytes())
 }
